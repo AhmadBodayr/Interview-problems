2ac0d52d295fd9bc1721eddd419fe772d9e4aa04
Ahmad Bodayr <ahmadbodayr@Ahmads-MacBook-Pro.local>
2023-08-21 21:25:27 +0300

    Solved

--- Regular Expression Matching.cpp
+++ Regular Expression Matching.cpp
@@ -4,62 +4,24 @@ using namespace std;
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int n = (int) p.size(), m = (int) s.size();
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, 0));
-        dp[0][0] = 1;
-        for(int i = 1; i <= n; i++) {
-            if(i == n) {
-                if(p[i - 1] == '*') dp[i][0] = 1;
-            } else {
-                if(p[i] == '*') dp[i][0] = 1;
-                else if(p[i - 1] == '*') dp[i][0] = 1;
-                else break;
+        int n = (int) s.size(), m = (int) p.size();
+        vector<vector<bool>>dp(n + 1, vector<bool>(m + 1, false));
+        dp[0][0] = true;
+        for (int i = 0; i < m; i++) {
+            if (p[i] == '*' && dp[0][i - 1]) {
+                dp[0][i + 1] = true;
             }
         }
-        for(int i = 1; i <= n; i++) {
-            if(p[i - 1] == '*') {
-                for(int j = 0; j <= m; j++) dp[i][j] = dp[i - 1][j];
-                continue;
-            }
-            if(i == n) {
-                if(p[i - 1] == '.') {
-                    for(int j = 1; j <= m; j++) if(dp[i - 1][j - 1]) {
-                        dp[i][j] = 1;
-                    } 
-                } else {
-                    for(int j = 1; j <= m; j++) {
-                        if(dp[i - 1][j - 1] && p[i - 1] == s[j - 1]) {
-                            dp[i][j] = 1;
-                        }
-                    }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (p[j] == '.' || s[i] == p[j]) {
+                    dp[i + 1][j + 1] = dp[i][j];
                 }
-            } else { // The problem
-                if(p[i] == '*') {
-                    if(p[i - 1] == '.') {
-                        for(int j = 1; j <= m; j++) {
-                            dp[i][j] = dp[i - 1][j] | dp[i][j - 1] | dp[i - 1][j - 1];
-                        }
+                if (p[j] == '*') {
+                    if (p[j - 1] != s[i] && p[j - 1] != '.') {
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
                     } else {
-                        for(int j = 1; j <= m; j++) {
-                            dp[i][j] = dp[i - 1][j];
-                            if(p[i - 1] == s[j - 1]) {
-                                if(dp[i - 1][j - 1] || dp[i][j - 1]) {
-                                    dp[i][j] = 1;
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    if(p[i - 1] == '.') {
-                        for(int j = 1; j <= m; j++) if(dp[i - 1][j - 1]) {
-                            dp[i][j] = 1;
-                        }
-                    } else {
-                        for(int j = 1; j <= m; j++) {
-                            if(dp[i - 1][j - 1] && p[i - 1] == s[j - 1]) {
-                                dp[i][j] = 1;
-                            }
-                        } 
+                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
                     }
                 }
             }
